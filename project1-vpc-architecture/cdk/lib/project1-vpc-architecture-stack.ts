cb3f4d091c1ea3c2d2a35d848e63f42a75992a6c
Nicolas Gloss <glossnicolas+cp@gmail.com>
2025-08-27 23:55:01 +1000

    feat: add initial VPC stack (public and private subnets, 2 AZs)

---
+++ project1-vpc-architecture/cdk/lib/project1-vpc-architecture-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+
+export class Project1VpcArchitectureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new ec2.Vpc(this, 'AppVpc', {
+      vpcName: 'proj1-vpc-architecture',
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        { name: 'public',  subnetType: ec2.SubnetType.PUBLIC,              cidrMask: 24 },
+        { name: 'private', subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS, cidrMask: 24 },
+      ],
+    });
+  }
+}
